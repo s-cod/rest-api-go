175737077e9eef7220ede92ffd855c01c3cd7c33
Ivan Sofronov <s-cod@gmail.com>
2024-05-28 15:20:38 +0600

    add Store postgres database from Docker

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 .PHONY: build
 build:
 	go build -v ./cmd/apiserver/
+run:
+	go run ./cmd/apiserver/
 
 .PHONY: db
 db-stop:
@@ -9,11 +11,11 @@ db:
 	docker compose up -d
 # run:
 # 	docker run --name api-go -e POSTGRES_DB=rest-api -e POSTGRES_USER=restapi -e POSTGRES_PASSWORD=restapi -v "${pwd}\database:/var/lib/postgresql/data" -d postgres
-	
+# db-psql:
+# docker exec -it 5d8583c47e4e psql -U restapi  rest-api
+
 db-stop:
 	docker compose stop
-# db-psql:
-# 	docker exec -it rest-api psql -U restapi restapi
 
 .PHONY: test
 test:
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/s-cod/rest-api/internal/app/store"
 	"github.com/sirupsen/logrus"
 )
 
@@ -13,6 +14,7 @@ type APIServer struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+	store  *store.Store
 }
 
 // New...
@@ -29,8 +31,13 @@ func (s *APIServer) Start() error {
 	if err := s.configureLogger(); err != nil {
 		return err
 	}
-	s.logger.Info("starting api server")
 	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
+
+	s.logger.Info("starting api server")
 	return http.ListenAndServe(s.config.BindAddr, s.router)
 
 }
@@ -53,4 +60,13 @@ func (s *APIServer) hanleHello() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "hello")
 	}
+}
+
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+	s.store = st
+	return nil
 }
