9b3ce9b58042c6f857fb7990079dc1952f19ac55
Ivan Sofronov <s-cod@gmail.com>
2024-06-04 10:17:26 +0600

    add test TestUserRepository_findById

--- internal/app/store/userrepository_test.go
+++ internal/app/store/userrepository_test.go
@@ -21,3 +21,21 @@ func TestUserRepository_Create(t *testing.T) {
 	assert.NotNil(t, u)
 
 }
+
+func TestUserRepository_FingByEmail(t *testing.T) {
+	s, teardown := store.TestStore(t, "host=localhost dbname=restapi_test user=restapi password=restapi sslmode=disable")
+
+	defer teardown("users")
+
+	email := "user@example.org"
+	_, err := s.User().FindByEmail(email)
+	assert.Error(t, err)
+
+	s.User().Create(&model.User{
+		Email:             "user@example.org",
+		EncryptedPassword: "test",
+	})
+	u, err := s.User().FindByEmail(email)
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+}
